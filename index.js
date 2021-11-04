326a1ddd2d26bc7d3fcdd7b53d5ad42a5f5e9d9e
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-11-04 11:00:56 -0400

    commit tree c2fab368bfad508b91c08b09fb0a9e7e702a2459

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const record = [
+    { year: "1970", result: "N/A"},
+    { year: "1969", result: "W"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"},
+    { year: "1966", result: "L"},
+  ]
+
+function findWin(win){
+    return win.result === 'W';
+}
+    function superbowlWin(array) {
+        if (array.find(findWin) === undefined){
+            return array.find(findWin)
+        }
+        else {
+	    return array.find(findWin).year
+    }
+}
+
+superbowlWin(record)
 
